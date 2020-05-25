eb35748ccd138642466f3c6b1b8b6d6e8354067b
root <root@ip-172-26-14-237.eu-west-2.compute.internal>
2020-05-25 19:03:20 +0000

    Repo Bludit

---
+++ databases/syslog.php
@@ -0,0 +1,11 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+[
+    {
+        "date": "2020-05-25 20:01:25",
+        "dictionaryKey": "welcome-to-bludit",
+        "notes": "",
+        "idExecution": "5ecc16055e2a1",
+        "method": "POST",
+        "username": "admin"
+    }
+]
---
+++ databases/users.php
@@ -0,0 +1,23 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{
+    "admin": {
+        "nickname": "Admin",
+        "firstName": "Administrador",
+        "lastName": "",
+        "role": "admin",
+        "password": "10879ae46f1fe150f1fefaf8a2837717678ea811",
+        "salt": "5ecc16055e242",
+        "email": "",
+        "registered": "2020-05-25 20:01:25",
+        "tokenRemember": "",
+        "tokenAuth": "f75810c7380573fb87087d54c9720a83",
+        "tokenAuthTTL": "2009-03-15 14:00",
+        "twitter": "",
+        "facebook": "",
+        "instagram": "",
+        "codepen": "",
+        "linkedin": "",
+        "github": "",
+        "gitlab": ""
+    }
+}
